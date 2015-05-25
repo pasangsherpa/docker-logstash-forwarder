ab423d240da993a6b9c7fc4e5df52a8d7d74df8c
Pasang Sherpa <pgsherpa12@gmail.com>
2015-05-25 00:56:07 -0700

    entrypoint script fixed to let users run the container with their own process

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,15 +5,20 @@ set -e
 # Add logstash-forwarder as command if needed
 if [[ "$1" == -* ]]; then
     set -- logstash-forwarder "$@"
+else 
+    # Run as user "logstash-forwarder" if the command is "logstash-forwarder"
+    if [ "$1" == 'logstash-forwarder' ]; then
+        set -- gosu logstash-forwarder "$@"
+    else
+        # As argument is not related to logstash-forwarder,
+        # then assume that user wants to run his own process,
+        # for example a `bash` shell to explore this image
+        exec "$@"
+    fi
 fi
 
-# Run as user "logstash-forwarder" if the command is "logstash-forwarder"
-if [ "$1" == logstash-forwarder ]; then
-    set -- gosu logstash-forwarder "$@"
-fi
-
-# Add default config if not set
-if [[ $* == *-config* ]]; then
+# Use default config if config is not provided
+if [[ "$*" == *-config* ]]; then
     exec "$@"
 else 
     exec "$@" -config /opt/conf/logstash-forwarder.conf
